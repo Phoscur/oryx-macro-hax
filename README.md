d1f27f7445659641771218399a1fe6c1c54d32df
gittyeric <git@letsmakeit.com>
2020-09-27 05:55:49 -0700

    init

---
+++ README.md
@@ -0,0 +1,41 @@
+# Ergodox Macro Hax
+
+This is a quick and dirty script to nicely get around the annoying (though reasonable) limitation of Ergodox / Moonlander keyboards in the Oryx configurator where macros can only be 4 key sequences for security reasons.
+
+This hack is a post-processor that'll take your raw "Source" Oryx configuration and extend your macros to be any arbitrary sequence.
+
+# Complete configuration flow
+
+## 1. Configuring with Oryx
+
+Configure in Oryx just as you normally would, except make sure each macro you'd like to extend beyond 4 button presses is a unique set of alphanumeric button presses that act as a unique ID to find in post-processing.  Let's say you want a keyboard to type "whale", you could just write a macro with keys "whal" or get really detailed with numeric IDs and type "1234".  Remember these IDs for later.  DO NOT use any key modifiers, custom delays or non-alphanumeric keys in your extendable macros, you can specify those yourself later.
+
+When done, download the "Source" (don't download the compiled binary), unzip it and note the directory where you unzipped it to.
+
+## 2. Create a mapping in this script
+
+Grab my-illicit-macros.ts and change it to what you need, starting with the absolute directory path `SOURCE_DIR` pointed to the unzipped directory that contains the "keymap.c" file (NOT usually in the root directory!) you noted in step 1.  Now open my-illicit-macros.ts and modify to match your configuration.
+
+Here's an example of creating an extended (more than 4 button) macro:
+
+```
+const macro = newMacro()
+    .typeAlphanumeric("function") // Supports a-z and 0-9
+    .sendRawCmd("SS_DELAY(100)") // Raw commands in the C file
+    .tapKey("X_SPACE") // Tap a raw key code
+    .delay(50) // Delay for 50 ms
+```
+
+Now just map the original 4 character macro to your newer, longer macro.  See what's already in my-illicit-macros.ts for fuller examples.
+
+## 3. Run the post-processor
+
+Run `npm run hax` to modify your config and extend your macros.
+
+## 4. Build Modified Source and Flash it!
+
+Use the normal ZSA Wally software to compile and flash.
+
+## Note: Figuring out key codes
+
+I'm too lazy to list them for you, when I need a keycode such as "X_SPACE", I just put it in a macro and find it in the keymap.c file, then reference that name from then on out.
---
+++ macros.ts
@@ -0,0 +1,166 @@
+import { readFileSync, writeFileSync } from "fs";
+
+export interface MacroBuilder {
+    delay: (milliseconds: number) => MacroBuilder,
+
+    sendRawCmd: (rawCmd: string) => MacroBuilder,
+
+    // Works with a-z, 0-9, space and newline-as-enter
+    typeAlphanumeric: (strToType: string, msDelayBetweenStrokes?: number) => MacroBuilder,
+
+    tapKey: (rawKey: string, msDelayAfter?: number) => MacroBuilder,
+
+    withModifiers: (innerMacro: MacroBuilder, rawModifiers: string[]) => MacroBuilder,
+
+    withShift: (innerMacro: MacroBuilder) => MacroBuilder,
+
+    build: () => string,
+
+    expectedReplacements: number,
+}
+
+export const newMacro: (expectedReplacementCount?: number) => MacroBuilder = (erc: number = 1) => {
+    const commands: (() => string)[] = []
+    const self: MacroBuilder = {
+        expectedReplacements: erc,
+        tapKey: (rawKey: string, msDelayAfter: number) => {
+            if (!rawKey.startsWith("X_")) {
+                throw new Error("rawKey must come from the underlying language thingy, maybe you meant X_" + rawKey.toUpperCase() + " or something?");
+            }
+            commands.push(() => `SS_TAP(${rawKey})`);
+            if (msDelayAfter > 0) {
+                self.delay(msDelayAfter)
+            }
+            return self;
+        },
+        // Works with a-z, 0-9, space and newline-as-enter
+        typeAlphanumeric: (strToType: string, msDelayBetweenStrokes: number = 30) => {
+            for (let i = 0; i < strToType.length; i++) {
+                const char = strToType[i]
+                if (char !== char.toLowerCase()) {
+                    self.withShift(newMacro().typeAlphanumeric(char.toLowerCase()))
+                }
+                else if (char !== char.toUpperCase() || (char >= "0" && char <= "9")) {
+                    self.tapKey(`X_${char.toUpperCase()}`, msDelayBetweenStrokes);
+                }
+                else if (char === " ") {
+                    self.tapKey("X_SPACE")
+                }
+                else if (char === "\n") {
+                    self.tapKey("X_ENTER")
+                }
+                else if (char === "=") {
+                    self.tapKey("X_EQUAL")
+                }
+                else if (char === "[") {
+                    self.tapKey("X_LBRACKET")
+                }
+                else if (char === "]") {
+                    self.tapKey("X_RBRACKET")
+                }
+                else if (char === ";") {
+                    self.tapKey("X_SEMICOLON")
+                }
+                else if (char === ",") {
+                    self.tapKey("X_COMMA")
+                }
+                else if (char === ".") {
+                    self.tapKey("X_DOT")
+                }
+                else if (char === "+") {
+                    self.tapKey("X_PLUS")
+                }
+                else if (char === "/") {
+                    self.tapKey("X_SLASH")
+                }
+                else {
+                    throw Error("Unsupported char in typeAlphanumeric: " + char + ", use typeRaw instead?")
+                }
+
+                if (msDelayBetweenStrokes > 0) {
+                    self.delay(msDelayBetweenStrokes);
+                }
+            }
+            return self;
+        },
+        delay: (msDelay: number) => {
+            self.sendRawCmd("SS_DELAY(" + msDelay + ")");
+            return self;
+        },
+        sendRawCmd: (rawCmd: string) => {
+            commands.push(() => rawCmd)
+            return self
+        },
+        // Runs some inner macro while holding down all
+        // modifier keys
+        withModifiers: (
+            innerMacro: MacroBuilder,
+            rawModifiers: string[]) => {
+            commands.push(() =>
+                rawModifiers.map((m) => `${m}(`) +
+                innerMacro.build() +
+                rawModifiers.map(() => ")"));
+            return self
+        },
+        withShift: (innerMacro: MacroBuilder) => {
+            return self.withModifiers(innerMacro, ["SS_LSFT"])
+        },
+        build: () => {
+            const cmds = commands.map((cmd) => cmd())
+            // Trim delays off the ends of commands
+            while (cmds.length > 0 && cmds[cmds.length-1].startsWith("SS_DELAY")) {
+                cmds.splice(cmds.length-1, 1)
+            }
+            return cmds.join(" ");
+        }
+    };
+
+    return self;
+}
+
+export const processAll = (macroMap: {
+    [originalMacroKeys: string]: MacroBuilder
+}, keymapFile: string) => {
+    const loaded = readFileSync(keymapFile).toString()
+    const orig = Object.keys(macroMap)
+    orig.forEach((macroKeys) => {
+        const toFind = "SEND_STRING(" + charStrToMacro(macroKeys).build() + ")"
+        const newMacro = macroMap[macroKeys]
+        const matchCount = loaded.split(toFind).length - 1
+        if (matchCount !== newMacro.expectedReplacements) {
+            console.error(toFind)
+            throw new Error(`Found ${matchCount} instances of the ${macroKeys} macro but expected ${newMacro.expectedReplacements} instances!  Check your config and set the proper value in newMacro()`)
+        }
+    })
+
+    const backup = keymapFile + Math.random() + ".old-"
+    console.log("Backed up keymap.c to " + backup)
+    writeFileSync(backup, loaded)
+
+    let newConfig = `${loaded}`
+    orig.forEach((macroKeys) => {
+        const toFind = "SEND_STRING(" + charStrToMacro(macroKeys).build() + ")"
+        const newMacro = macroMap[macroKeys]
+        const macro = "SEND_STRING(" + newMacro.build() + ")"
+        for (let i = 0; i < newMacro.expectedReplacements; i++) {
+            console.log("Replacing \n" + toFind + "\nwith\n" + macro)
+            newConfig = newConfig.replace(toFind, macro)
+        }
+    })
+    writeFileSync(keymapFile, newConfig)
+    console.log("ALL done! Proceed with compilation and flashing")
+};
+
+function charStrToMacro(keys: string): MacroBuilder {
+    if (keys.length < 1 || keys.length > 4) {
+        throw new Error("Please check macro ID for " + keys);
+    }
+    let macro = newMacro()
+    for (let i = 0; i < keys.length; i++) {
+        macro = macro.tapKey(`X_${keys[i].toUpperCase()}`)
+        if (i < keys.length - 1) {
+            macro = macro.delay(100)
+        }
+    }
+    return macro
+}
