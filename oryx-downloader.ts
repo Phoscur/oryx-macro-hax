18d44507e1f8f934637a3d1abc8208327e88e2e4
Phoscur <phoscur@pheelgood.net>
2021-12-31 22:13:12 +0100

    Add graphql query to get zipUrl

--- oryx-downloader.ts
+++ oryx-downloader.ts
@@ -1,13 +1,31 @@
-import fs from "fs"
-import axios from "axios"
-import unzipper from "unzipper"
 
-axios({
-  method: 'get',
-  url: "https://oryx.zsa.io/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBMW1ySlE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--7dd4521b1d18db272247e91d3829130918d98039/moonlander_neophil-autobahn-fueled-wip_PqjlE_aVPD7.zip",
-  // 'http://bit.ly/2mTM3nY',
-  responseType: 'stream'
-})
-  .then(function (response) {
-    response.data.pipe(unzipper.Extract({ path: './test/neophil' }))//fs.createWriteStream('ada_lovelace.jpg'))
-  });
+import axios from 'axios'
+import unzipper from 'unzipper'
+
+function getSource(url) {
+  axios({
+    method: 'get',
+    url,
+    responseType: 'stream'
+  })
+    .then(function (response) {
+      response.data.pipe(unzipper.Extract({ path: './test/neophil' }))
+    });
+}
+
+const ORYX_GRAPHQL_URL = 'https://oryx.zsa.io/graphql'
+async function getSourceLink(layoutName) {
+  const { data } = await axios.post(ORYX_GRAPHQL_URL, {
+    operationName: 'getLayout',
+    variables: {
+        hashId: layoutName,
+        geometry: 'moonlander',
+        revisionId: 'latest'
+    },
+    query: `query getLayout($hashId: String!, $revisionId: String!, $geometry: String) {\n  Layout(hashId: $hashId, geometry: $geometry, revisionId: $revisionId) {\n    ...LayoutData\n    __typename\n  }\n}\n\nfragment LayoutData on Layout {\n  privacy\n  geometry\n  hashId\n  parent {\n    hashId\n    __typename\n  }\n  tags {\n    id\n    hashId\n    name\n    __typename\n  }\n  title\n  user {\n    annotationPublic\n    name\n    hashId\n    pictureUrl\n    __typename\n  }\n  revision {\n    ...RevisionData\n    __typename\n  }\n  __typename\n}\n\nfragment RevisionData on Revision {\n  aboutIntro\n  aboutOutro\n  createdAt\n  hashId\n  hexUrl\n  model\n  title\n  config\n  swatch\n  zipUrl\n  qmkVersion\n  qmkUptodate\n  layers {\n    builtIn\n    hashId\n    keys\n    position\n    title\n    color\n    __typename\n  }\n  __typename\n}\n`
+  })
+  console.log('Zip url:', data.data.Layout.revision.zipUrl)
+  return data.data.Layout.revision.zipUrl
+}
+
+getSourceLink('PqjlE').then(getSource)
