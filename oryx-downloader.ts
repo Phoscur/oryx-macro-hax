67b7af9af6dee91bd2f8ffa44ca5410e05acb74c
Phoscur <phoscur@pheelgood.net>
2022-01-01 00:38:25 +0100

    Refactor expand layout graphql query

--- oryx-downloader.ts
+++ oryx-downloader.ts
@@ -14,15 +14,77 @@ function getSource(url) {
 }
 
 const ORYX_GRAPHQL_URL = 'https://oryx.zsa.io/graphql'
-async function getSourceLink(layoutName) {
+async function getSourceLink(layoutName, revisionId = 'latest') {
+  const query = `
+  query getLayout($hashId: String!, $revisionId: String!, $geometry: String) {
+      Layout(hashId: $hashId, geometry: $geometry, revisionId: $revisionId) {
+        ...LayoutData
+        __typename
+      }
+    }
+
+  fragment LayoutData on Layout {
+    privacy
+    geometry
+    hashId
+    parent {
+      hashId
+      __typename
+    }
+    tags {
+      id
+      hashId
+      name
+      __typename
+    }
+    title
+    user {
+      annotationPublic 
+      name
+      hashId
+      pictureUrl
+    __typename
+    }
+    revision {
+      ...RevisionData
+      __typename
+    }
+    __typename
+  }
+
+  fragment RevisionData on Revision {
+    aboutIntro
+    aboutOutro
+    createdAt
+    hashId
+    hexUrl
+    model
+    title
+    config
+    swatch
+    zipUrl
+    qmkVersion
+    qmkUptodate
+    layers {
+      builtIn
+      hashId
+      keys
+      position
+      title
+      color
+      __typename
+    }
+    __typename
+  }
+`;
   const { data } = await axios.post(ORYX_GRAPHQL_URL, {
     operationName: 'getLayout',
     variables: {
         hashId: layoutName,
         geometry: 'moonlander',
-        revisionId: 'latest'
+        revisionId,
     },
-    query: `query getLayout($hashId: String!, $revisionId: String!, $geometry: String) {\n  Layout(hashId: $hashId, geometry: $geometry, revisionId: $revisionId) {\n    ...LayoutData\n    __typename\n  }\n}\n\nfragment LayoutData on Layout {\n  privacy\n  geometry\n  hashId\n  parent {\n    hashId\n    __typename\n  }\n  tags {\n    id\n    hashId\n    name\n    __typename\n  }\n  title\n  user {\n    annotationPublic\n    name\n    hashId\n    pictureUrl\n    __typename\n  }\n  revision {\n    ...RevisionData\n    __typename\n  }\n  __typename\n}\n\nfragment RevisionData on Revision {\n  aboutIntro\n  aboutOutro\n  createdAt\n  hashId\n  hexUrl\n  model\n  title\n  config\n  swatch\n  zipUrl\n  qmkVersion\n  qmkUptodate\n  layers {\n    builtIn\n    hashId\n    keys\n    position\n    title\n    color\n    __typename\n  }\n  __typename\n}\n`
+    query,
   })
   console.log('Zip url:', data.data.Layout.revision.zipUrl)
   return data.data.Layout.revision.zipUrl
