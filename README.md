551cb6aabeefaee827b38dcb88010b9d8039cc34
Phoscur <phoscur@pheelgood.net>
2022-01-21 23:42:17 +0100

    Add ORYX Downloader and Github QMK Compile Action Pipeline

--- README.md
+++ README.md
@@ -6,20 +6,36 @@ This hack is a post-processor that'll take your raw "Source" Oryx configuration 
 
 # Complete configuration flow
 
+## 0. Initial setup
+
+To compile the firmware, setup the [ZSA QMK Fork with guide referenced](https://github.com/zsa/qmk_firmware)
+
+`qmk setup zsa/qmk_firmware -b firmware20`
+
+Install on the command line (with Node.js installed) using:
+```
+cd ~/qmk_firmware
+git clone <your fork of this repo>
+cd ergodox-macro-hax
+npm install
+```
+
+
 ## 1. Configuring with Oryx
 
 Configure in Oryx just as you normally would, except make sure each macro you'd like to extend beyond 4 button presses is a unique set of alphanumeric button presses that act as a unique ID to find in post-processing.  Let's say you want a keyboard to type "whale", you could just write a macro with keys "whal" or get really detailed with numeric IDs and type "1234".  Remember these IDs for later.  DO NOT use any key modifiers, custom delays or non-alphanumeric keys in your extendable macros, you can specify those yourself later.
 
-When done, download the "Source" (don't download the compiled binary), unzip it and note the directory where you unzipped it to.
+When done, take a note of the `layout hash ID` in the URL or download the source manually into the `keymap_src` folder.
+
+It is also recommended to `git branch <your layout name>` and gitignore allowlist `!layout_src/<your layout name>`.
 
 ## 2. Create a mapping in this script
 
-Install on the command line (with Node.js installed) using:
 
 ```
-cd path/to/ergodox-macro-hax
+cd ~/qmk_firmware/ergodox-macro-hax
 
-npm install
+npm run get -- <oryxLayoutHashId>
 ```
 
 Grab my-illicit-macros.ts and change it to what you need, starting with the absolute directory path `SOURCE_DIR` pointed to the unzipped directory that contains the "keymap.c" file (NOT usually in the root directory!) you noted in step 1.  Now open my-illicit-macros.ts and modify to match your configuration.
@@ -39,10 +55,16 @@ Now just map the original 4 character macro to your newer, longer macro.  See wh
 ## 3. Run the post-processor
 
 Run `npm run hax` to modify your config and extend your macros.
+Run `npm run copy` to copy the processed keymap to the parent keymaps folder (keymaps/neo).
 
 ## 4. Build Modified Source and Flash it!
 
-Use the normal ZSA Wally software to compile and flash.
+Use the normal QMK/ZSA Wally software to compile and flash.
+
+`qmk compile -kb moonlander -km neo`
+
+If you want Github to build the firmware for you, create repository action secrets `LAYOUT_ID`, set it to the hashId of your layout keymap, as well as `LAYOUT_FOLDER` with the name of the folder of your keymap in the source archive.
+(Extract it from your ORYX link: `https://configure.zsa.io/moonlander/layouts/<hashId>/latest/0`) then you can use [Github Actions](./.github/workflows/process.yml) to build the firmware for you.
 
 ## Note: Figuring out key codes
 
---
+++ my-macros.ts
@@ -0,0 +1,169 @@
+import { newMacro, processAll } from "./macros";
+
+// Point this at wherever your config root folder is
+const SOURCE_DIR = "./layout_src/moonlander_neophil-autobahn-fueled-wip_source/keymap.c";
+
+const macroExtensions = {
+
+    // Application quick launches
+    /*"fire": newMacro()
+        .tapKey("X_LGUI", 100)
+        .typeAlphanumeric("firefox\n"),*/
+    "dev": newMacro()
+        .tapKey("X_LGUI", 100)
+        .typeAlphanumeric("visual studio c\n"),
+
+    "poke": newMacro()
+        .withShift(newMacro().typeAlphanumeric("1")) // ^1 = !
+        .typeAlphanumeric("pokecatch "),
+
+    "gg": newMacro()
+        .tapKey("X_ENTER")
+        .withShift(newMacro().typeAlphanumeric("8gg9")) // ^8 = (, ^9 = )
+        .tapKey("X_ENTER"),
+
+    // Git shortcuts
+    /*"add": newMacro(0)
+        .typeAlphanumeric("git add -A"),
+    "comm": newMacro(1)
+        .typeAlphanumeric("git commit -m \"\"")
+        .tapKey("X_LEFT"),
+    "push": newMacro(1)
+        .typeAlphanumeric("git push"),
+    //*/
+    // Macros for Typescript mode, type full keywords beyond 4 characters
+    /*"cons": newMacro()
+        .typeAlphanumeric("const "),
+    "retu": newMacro()
+        .typeAlphanumeric("return "),
+    "whil": newMacro()
+        .typeAlphanumeric("while ")
+        // Open and closing parenthesis and brackets
+        .withShift(newMacro().typeAlphanumeric("90 []"))
+        .tapKey("X_LEFT")
+        .tapKey("X_ENTER")
+        .tapKey("X_UP")
+        .withShift(newMacro().tapKey("X_END"))
+        .tapKey("X_LEFT")
+        .tapKey("X_LEFT")
+        .tapKey("X_LEFT"),
+    "regx": newMacro()
+        .typeAlphanumeric("//g")
+        .tapKey("X_LEFT"),
+    "if": newMacro()
+        .typeAlphanumeric("if ")
+        // Open and closing parenthesis
+        .withShift(newMacro().typeAlphanumeric("90 []"))
+        .tapKey("X_LEFT")
+        .tapKey("X_ENTER")
+        .tapKey("X_UP")
+        .withShift(newMacro().tapKey("X_END"))
+        .tapKey("X_LEFT")
+        .tapKey("X_LEFT")
+        .tapKey("X_LEFT"),
+    "elif": newMacro()
+        .typeAlphanumeric("else if ")
+        // Open and closing parenthesis
+        .withShift(newMacro().typeAlphanumeric("90 []"))
+        .tapKey("X_LEFT")
+        .tapKey("X_ENTER")
+        .tapKey("X_UP")
+        .withShift(newMacro().tapKey("X_END"))
+        .tapKey("X_LEFT")
+        .tapKey("X_LEFT")
+        .tapKey("X_LEFT"),
+    "else": newMacro()
+        .typeAlphanumeric("else ")
+        // Open and closing parenthesis
+        .withShift(newMacro().tapKey("X_LBRACKET").tapKey("X_RBRACKET"))
+        .tapKey("X_LEFT")
+        .tapKey("X_ENTER"),
+    "expo": newMacro()
+        .typeAlphanumeric("export "),
+    // .forEach((el) => {})
+    "fore": newMacro()
+        .typeAlphanumeric(".forEach")
+        .withShift(newMacro().typeAlphanumeric("e"))
+        .typeAlphanumeric("ach")
+        // Open and closing parenthesis
+        .withShift(newMacro().typeAlphanumeric("90"))
+        .tapKey("X_LEFT")
+        .withShift(newMacro().typeAlphanumeric("90"))
+        .tapKey("X_LEFT")
+        .typeAlphanumeric("el")
+        .tapKey("X_RIGHT")
+        .tapKey("X_SPACE")
+        .tapKey("X_EQUAL")
+        // Type >
+        .withShift(newMacro().tapKey("X_DOT"))
+        .tapKey("X_SPACE")
+        .withShift(newMacro().tapKey("X_LBRACKET").tapKey("X_RBRACKET"))
+        .tapKey("X_LEFT")
+        .tapKey("X_ENTER"),
+    "map": newMacro()
+        .typeAlphanumeric(".map")
+        // Open and closing parenthesis
+        .withShift(newMacro().typeAlphanumeric("90"))
+        .tapKey("X_LEFT")
+        .withShift(newMacro().typeAlphanumeric("90"))
+        .tapKey("X_LEFT")
+        .typeAlphanumeric("el")
+        .tapKey("X_RIGHT")
+        .typeAlphanumeric(" =")
+        // Type >
+        .withShift(newMacro().tapKey("X_DOT"))
+        .tapKey("X_SPACE")
+        .withShift(newMacro().tapKey("X_LBRACKET").tapKey("X_RBRACKET"))
+        .tapKey("X_LEFT")
+        .typeAlphanumeric("\nreturn "),
+    "filt": newMacro()
+        .tapKey("X_DOT")
+        .typeAlphanumeric("filter")
+        // Open and closing parenthesis
+        .withShift(newMacro().typeAlphanumeric("90"))
+        .tapKey("X_LEFT")
+        .withShift(newMacro().typeAlphanumeric("90"))
+        .tapKey("X_LEFT")
+        .typeAlphanumeric("el")
+        .tapKey("X_RIGHT")
+        .typeAlphanumeric(" =")
+        // Type >
+        .withShift(newMacro().tapKey("X_DOT"))
+        .withShift(newMacro().typeAlphanumeric(" []"))
+        .tapKey("X_LEFT")
+        .typeAlphanumeric("\nreturn "),
+    "func": newMacro()
+        .typeAlphanumeric("function ")
+        .withShift(newMacro().typeAlphanumeric("90 []"))
+        .tapKey("X_LEFT")
+        .typeAlphanumeric("\n")
+        .tapKey("X_UP")
+        .tapKey("X_END")
+        .tapKey("X_LEFT")
+        .tapKey("X_LEFT")
+        .tapKey("X_LEFT")
+        .withShift(newMacro().typeAlphanumeric("; ")),
+    "type": newMacro()
+        .typeAlphanumeric("type "),
+    "awai": newMacro()
+        .typeAlphanumeric("await "),
+    "asyn": newMacro()
+        .typeAlphanumeric("async "),
+    "bool": newMacro()
+        .typeAlphanumeric("boolean"),
+    "inte": newMacro()
+        .typeAlphanumeric("interface "),
+    "date": newMacro()
+        .typeAlphanumeric("Date"),
+    "prom": newMacro()
+        // Type a capital P
+        .typeAlphanumeric("Promise")
+        // Shift for a <>
+        .withShift(newMacro().tapKey("X_COMMA").tapKey("X_DOT"))
+        .tapKey("X_LEFT"),
+    "numb": newMacro().typeAlphanumeric("number"),
+    "stri": newMacro().typeAlphanumeric("string"),
+    //*/
+}
+
+processAll(macroExtensions, SOURCE_DIR)
