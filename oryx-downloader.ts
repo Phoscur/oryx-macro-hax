78e4c9f956b1c0c2d9323865353c4cdf48ae9b19
Phoscur <phoscur@pheelgood.net>
2023-04-08 21:49:14 +0200

    Fix oryx downloader

--- oryx-downloader.ts
+++ oryx-downloader.ts
@@ -1,13 +1,16 @@
 
 import axios from 'axios'
+import { existsSync } from 'fs'
+import { join } from 'path'
 import unzipper from 'unzipper'
 
 const ORYX_GRAPHQL_URL = 'https://oryx.zsa.io/graphql'
 
+
 export async function getKeymapSourceLink(hashId, revisionId = 'latest') {
   const query = `
 query getLayout($hashId: String!, $revisionId: String!, $geometry: String) {
-  Layout(hashId: $hashId, geometry: $geometry, revisionId: $revisionId) {
+  layout(hashId: $hashId, geometry: $geometry, revisionId: $revisionId) {
     revision {
       zipUrl
     }
@@ -22,7 +25,7 @@ query getLayout($hashId: String!, $revisionId: String!, $geometry: String) {
     },
     query,
   })
-  return data.data.Layout.revision.zipUrl
+  return data.data.layout.revision.zipUrl
 }
 
 export async function unzipKeymapSource(url, path) {
@@ -39,11 +42,19 @@ export async function unzipKeymapSource(url, path) {
 export async function downloadKeymapSource(layoutHashId, path) {
   const zipUrl = await getKeymapSourceLink(layoutHashId)
   await unzipKeymapSource(zipUrl, path)
-  console.log('Downloaded layout', layoutHashId, 'from', zipUrl, 'to', path)
+  console.log(`Downloaded layout "${layoutHashId}" to ${path} (from ${zipUrl}).`)
 }
 
 if (typeof require !== 'undefined' && require.main === module) {
-  const hashId = process.argv[2] || process.env.LAYOUT_ID;
-  const folder = process.argv[3] || process.env.LAYOUT_DIR;
-  downloadKeymapSource(hashId, folder);
+  const hashId = process.argv[2] || process.env.LAYOUT_ID
+  const parentFolder = process.argv[3] || process.env.LAYOUT_DIR || './layout_src'
+  const layoutFolder = process.argv[4] || process.env.LAYOUT_FOLDER
+  downloadKeymapSource(hashId, parentFolder).then(() => {
+    if (!existsSync(join(parentFolder, layoutFolder))) {
+      console.warn(`WARNING: Please check your environment: configured LAYOUT_FOLDER was not found: ${parentFolder}/${layoutFolder}`);
+    } else {
+      console.log('Unpacked folder:', layoutFolder);
+    }
+  });
+  
 }
