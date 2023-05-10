8450a3f6bd6fbf4cd1428d289bdc49e16cc1f3fc
Phoscur <phoscur@pheelgood.net>
2023-05-10 23:32:49 +0200

    Bring it back and disable just a few no-unsafe eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,15 @@ module.exports = {
   extends: [
     'eslint:recommended', 
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
+  rules: {
+    // no-unsafe does not play well with unzipper, prompts or node-persist
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+  },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   parserOptions: {
